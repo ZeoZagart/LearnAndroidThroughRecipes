32cd734bf03e0e845da4bcee32a7a78079ceb257
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-08-07 14:01:26 +0530

    added onConflictStrategy as Replace on DB insert

--- app/src/main/java/com/example/myapplication/DBFunctions.kt
+++ app/src/main/java/com/example/myapplication/DBFunctions.kt
@@ -2,17 +2,18 @@ package com.example.myapplication
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import io.reactivex.Flowable
+import io.reactivex.Single
 
 @Dao
 interface DBFunctions {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertSearchResults(dbRecipeSearchResults: List<DBRecipeSearchResult>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertRecipes(recipeList: List<DBRecipe>)
 
     @Query("SELECT DBRecipe.title,href,ingredients,thumbnail FROM DBRecipeSearchResult INNER JOIN DBRecipe ON DBRecipe.title = DBRecipeSearchResult.title WHERE ingredient IS :ingredient")
-    fun fetchRecipeNameList(ingredient: String): Flowable<List<DBRecipe>>
+    fun fetchRecipeNameList(ingredient: String): Single<List<DBRecipe>>
 }

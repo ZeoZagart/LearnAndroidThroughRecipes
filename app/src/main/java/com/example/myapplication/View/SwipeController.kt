55bcceddad9764bb2790e6153ff5bf7ca926f3db
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-08-09 14:39:41 +0530

    Add swipe controlled to support delete item by swiping

--- app/src/main/java/com/example/myapplication/View/SwipeController.kt
+++ app/src/main/java/com/example/myapplication/View/SwipeController.kt
@@ -1,2 +1,24 @@
 package com.example.myapplication.View
 
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+class SwipeController(val onSwipeCallbackFunction: (String, Int) -> Unit) : ItemTouchHelper.Callback() {
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        return makeMovementFlags(0, ItemTouchHelper.LEFT)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        onSwipeCallbackFunction((viewHolder as RecipeViewHolder).mItemView.text.toString(), viewHolder.adapterPosition)
+        println("Swiped " + viewHolder.mItemView.text + "++")
+    }
+
+}

437c080704e03b5fc02664cd97a857364ae28425
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-08-09 14:27:05 +0530

    DBFunctions move to Database folder

--- app/src/main/java/com/example/myapplication/Database/DBFunctions.kt
+++ app/src/main/java/com/example/myapplication/Database/DBFunctions.kt
@@ -1,9 +1,8 @@
 package com.example.myapplication
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.example.myapplication.Database.DBRecipe
+import com.example.myapplication.Database.DBRecipeSearchResult
 import io.reactivex.Single
 
 @Dao
@@ -14,6 +13,18 @@ interface DBFunctions {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertRecipes(recipeList: List<DBRecipe>)
 
-    @Query("SELECT DBRecipe.title,href,ingredients,thumbnail FROM DBRecipeSearchResult INNER JOIN DBRecipe ON DBRecipe.title = DBRecipeSearchResult.title WHERE ingredient IS :ingredient")
+    @Query("DELETE FROM DBRecipe WHERE title = :title")
+    fun deleteRecipeDBRecipe(title: String)
+
+    @Query("DELETE FROM DBRecipeSearchResult WHERE title = :title")
+    fun deleteRecipeDBRecipeSearchResult(title: String)
+
+    @Transaction
+    fun deleteRecipe(title: String){
+        deleteRecipeDBRecipeSearchResult(title)
+        deleteRecipeDBRecipe(title)
+    }
+
+    @Query("SELECT DBRecipe.title,href,ingredients,thumbnail FROM DBRecipeSearchResult INNER JOIN DBRecipe ON DBRecipe.title = DBRecipeSearchResult.title WHERE ingredient = :ingredient")
     fun fetchRecipeNameList(ingredient: String): Single<List<DBRecipe>>
 }

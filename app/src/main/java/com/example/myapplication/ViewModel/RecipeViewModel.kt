94775e7454a76239128021417069a7c465090469
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-08-19 12:19:58 +0530

    Refactor ViewModel

--- app/src/main/java/com/example/myapplication/ViewModel/RecipeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ViewModel/RecipeViewModel.kt
@@ -12,16 +12,11 @@ import io.reactivex.schedulers.Schedulers
 
 data class PuppyResult(val title: String, val version: Float, val href: String, val results: List<DBRecipe>)
 
-class RecipeViewModel(val recipeStore: DatabaseStore, val puppyServe: PuppyService) : ViewModel() {
+class RecipeViewModel(private val recipeStore: DatabaseStore, private val puppyServe: PuppyService) : ViewModel() {
     init {
         //application.deleteDatabase("recipe.db")
         println("RecipeViewModel Created!")
     }
-
-
-    //private val recipeStore = DatabaseStore(RecipeDatabase.getDatabase(application.applicationContext).DBFunctions())
-    //private val puppyServe: PuppyService = Networking.create().create(PuppyService::class.java)
-
 
     fun getData(ingredient: String, networkRefresh: Boolean): Flowable<List<DBRecipe>> = recipeStore.get(ingredient)
 
@@ -31,11 +26,10 @@ class RecipeViewModel(val recipeStore: DatabaseStore, val puppyServe: PuppyServi
     fun fetchAndInsertItems(ingredient: String): Disposable? {
         return puppyServe.getRecipeList(ingredient).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread()).subscribe(
-            { result: PuppyResult -> recipeStore.put(ingredient, result.results).subscribe() },
-            { println("Error fetching data from the internet : " + it.message.toString()) }
-        )
+                { result: PuppyResult -> recipeStore.put(ingredient, result.results).subscribe() },
+                { println("Error fetching data from the internet : " + it.message.toString()) }
+            )
     }
-
 
 
     override fun onCleared() {

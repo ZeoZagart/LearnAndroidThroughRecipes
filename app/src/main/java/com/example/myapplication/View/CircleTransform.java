575617318a2ce88a2a6effba4e0179d61427c302
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-08-20 16:45:55 +0530

    Apply curvature on Image Top Border

--- app/src/main/java/com/example/myapplication/View/CircleTransform.java
+++ app/src/main/java/com/example/myapplication/View/CircleTransform.java
@@ -7,6 +7,64 @@ public class CircleTransform implements Transformation {
     private final int BORDER_COLOR = Color.rgb(90, 80, 135);
     private final int BORDER_RADIUS = 0;
 
+    public static Path RoundedRect(
+            float left, float top, float right, float bottom, float rx, float ry,
+            boolean tl, boolean tr, boolean br, boolean bl
+    ) {
+        Path path = new Path();
+        if (rx < 0) rx = 0;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width / 2) rx = width / 2;
+        if (ry > height / 2) ry = height / 2;
+        float widthMinusCorners = (width - (2 * rx));
+        float heightMinusCorners = (height - (2 * ry));
+
+        path.moveTo(right, top + ry);
+        if (tr)
+            path.rQuadTo(0, -ry, -rx, -ry);//top-right corner
+        else {
+            path.rLineTo(0, -ry);
+            path.rLineTo(-rx, 0);
+        }
+        path.rLineTo(-widthMinusCorners, 0);
+        if (tl)
+            path.rQuadTo(-rx, 0, -rx, ry); //top-left corner
+        else {
+            path.rLineTo(-rx, 0);
+            path.rLineTo(0, ry);
+        }
+        path.rLineTo(0, heightMinusCorners);
+
+        if (bl)
+            path.rQuadTo(0, ry, rx, ry);//bottom-left corner
+        else {
+            path.rLineTo(0, ry);
+            path.rLineTo(rx, 0);
+        }
+
+        path.rLineTo(widthMinusCorners, 0);
+        if (br)
+            path.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
+        else {
+            path.rLineTo(rx, 0);
+            path.rLineTo(0, -ry);
+        }
+
+        path.rLineTo(0, -heightMinusCorners);
+
+        path.close();//Given close, last lineto can be removed.
+
+        return path;
+    }
+
+
+    @Override
+    public String key() {
+        return "circle";
+    }
+
     @Override
     public Bitmap transform(Bitmap source) {
         int size = Math.min(source.getWidth(), source.getHeight());
@@ -27,7 +85,7 @@ public class CircleTransform implements Transformation {
         paint.setShader(shader);
         paint.setAntiAlias(true);
 
-        float r = size / 2f;
+        float r = size / 20f;
 
         // Prepare the background
         Paint paintBg = new Paint();
@@ -35,17 +93,20 @@ public class CircleTransform implements Transformation {
         paintBg.setAntiAlias(true);
 
         // Draw the background circle
-        canvas.drawCircle(r, r, r, paintBg);
+        //canvas.drawCircle(r, r, r, paintBg);
 
         // Draw the image smaller than the background so a little border will be seen
-        canvas.drawCircle(r, r, r - BORDER_RADIUS, paint);
+
+
+        Path path = RoundedRect(0, 0, source.getWidth(), source.getHeight(), 20, 15, true, true, false, false);
+        //canvas.drawRoundRect(new RectF(0, 0, source.getWidth(), source.getHeight()), 0, r - BORDER_RADIUS, paint);
+        canvas.drawPath(path, paint);
+
 
         squaredBitmap.recycle();
         return bitmap;
     }
 
-    @Override
-    public String key() {
-        return "circle";
-    }
+
 }
+
